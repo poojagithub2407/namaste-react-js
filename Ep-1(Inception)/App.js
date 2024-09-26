f972f317bbdf246f8b6b88986cb7548faf16203c
poojadayal <poojaadayal@gmail.com>
2024-09-26 10:31:07 +0530

    first commit
    Signed-off-by: poojadayal <poojaadayal@gmail.com>

---
+++ Ep-1(Inception)/App.js
@@ -0,0 +1,17 @@
+const heading = React.createElement('h1', {}, 'hello word');
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
+
+
+const parent = React.createElement('div', { id: 'parent' }, React.createElement('div', { id: 'child' }, React.createElement('h1', {}, 'Hello from namaste react js')));
+
+root.render(parent);
+
+const sibling = React.createElement('div', { id: 'parent' }, React.createElement('div', { id: 'child' }, [
+     React.createElement('h1', { id: 'sibling1' }, 'sibling is first'),
+
+     React.createElement('h1', { id: 'sibling2' }, 'sibling is second')
+
+]))
+root.render(sibling)
