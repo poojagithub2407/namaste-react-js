e1bdfd940ee335de826762517a8a955fcc16330d
poojadayal <poojaadayal@gmail.com>
2024-09-30 10:46:57 +0530

    first commit
    Signed-off-by: poojadayal <poojaadayal@gmail.com>

--- Ep-1(Inception)/App.js
+++ Ep-1(Inception)/App.js
@@ -4,14 +4,16 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
 
 
-const parent = React.createElement('div', { id: 'parent' }, React.createElement('div', { id: 'child' }, React.createElement('h1', {}, 'Hello from namaste react js')));
+const parent = React.createElement('div', { id: 'parent' },
+     React.createElement('div', { id: 'child' },
+          React.createElement('h1', {}, 'Hello from namaste react js')));
 
 root.render(parent);
 
-const sibling = React.createElement('div', { id: 'parent' }, React.createElement('div', { id: 'child' }, [
-     React.createElement('h1', { id: 'sibling1' }, 'sibling is first'),
+const sibling = React.createElement('div', { id: 'parent' },
+     React.createElement('div', { id: 'child' }, [
+          React.createElement('h1', { id: 'sibling1' }, 'sibling is first'),
+          React.createElement('h1', { id: 'sibling2' }, 'sibling is second')
 
-     React.createElement('h1', { id: 'sibling2' }, 'sibling is second')
-
-]))
+     ]))
 root.render(sibling)
